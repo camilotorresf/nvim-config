318cd4833308dc9d1ceae5c7b23b9a0fd68b81f7
camilo.torres.ts <camilo.torres.ts@mixi.co.jp>
2022-09-14 16:46:15 -0400

    Add typescript debugging capabilities.

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -18,6 +18,30 @@ dap_install.setup {}
 dap_install.config("python", {})
 -- add other configs here
 
+dap.adapters.node2 = {
+  type = "executable",
+  command = "node",
+  args = { os.getenv('HOME') .. "/vscode-node-debug2/out/src/nodeDebug.js" },
+}
+dap.configurations.typescript = {
+  {
+    name = "Launch",
+    type = "node2",
+    request = "launch",
+    program = "$file",
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+    console = "integratedTerminal",
+  },
+  {
+    name = "Attach to process",
+    type = "node2",
+    request = "attach",
+    processId = require'dap.utils'.pick_process,
+  },
+}
+
 dapui.setup {
   sidebar = {
     elements = {
